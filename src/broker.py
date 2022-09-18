ddcf53ff55c3502734b9e9e52de7bc574424828a
Gitakkkk <stom1028@naver.com>
2022-09-19 01:12:50 +0900

    delete: 네이버 지도 크롤링 실패, 추후 selenium 사용해서 재도전

--- src/broker.py
+++ src/broker.py
@@ -1,23 +1,26 @@
-import requests
-import re
-from bs4 import BeautifulSoup as bs
+# import requests
+# import re
+# from bs4 import BeautifulSoup as bs
 
-# res = requests.get('https://map.naver.com/v5/search/%EA%B2%BD%EB%A7%A4?c=14317597.4804258,4282334.7073563,14.16,0,0,0,dh')
+# url = 'https://map.naver.com/v5/search/%EA%B2%BD%EB%A7%A4?c=14317597.4804258,4282334.7073563,14.16,0,0,0,dh'
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+# res = requests.get(url, headers=headers)
 # res.raise_for_status()
 
-# soup = bs(res.text, 'lxml')
-# print(soup)
+# soup = bs(res.content, 'lxml')
+# # print(soup)
 
-# titles = soup.find_all('span', attrs={'class':'place_bluelink YwYLL'})
+# titles = soup.find_all('span', attrs={'class':re.compile('^place_bluelink')})
 # print(titles)
 
-res = requests.get('https://media.naver.com/press/055')
-res.raise_for_status()
+# res = requests.get('https://media.naver.com/press/055')
+# res.raise_for_status()
 
-soup = bs(res.text, 'lxml')
-# print(soup)
+# soup = bs(res.content, 'lxml')
+# # print(soup)
 
-titles = soup.find_all('span', attrs={'class':'press_news_text'})
+# titles = soup.find_all('span', attrs={'class':re.compile('press_news_text')})
+# # print(titles)
 
-for title in titles:
-    print(title.strong.text)
+# for title in titles:
+#     print(title.strong.text)
