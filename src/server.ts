e90b4a65cda41ab7049c43c25393735b9544c5fc
Gitakkkk <stom1028@naver.com>
2022-09-24 22:36:27 +0900

    fix: get to '/' redirect homepage

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ const options = {
 
 app.get('/', (req: Request, res: Response) => {
   res.send('<script>alert("올바르지 않은 접근입니다.")</script>');
-  res.redirect('https://www.gmmoa.com');
+  return res.redirect('https://www.gmmoa.com');
 });
 
 app.use('/api', subs_router);
