61c8181aa1cb4ac07c71523446739112569c9edf
Gitakkkk <stom1028@naver.com>
2022-09-24 22:37:16 +0900

    fix: get to '/' redirect homepage

--- dist/src/server.js
+++ dist/src/server.js
@@ -51,7 +51,7 @@ const options = {
 // );
 app.get('/', (req, res) => {
     res.send('<script>alert("올바르지 않은 접근입니다.")</script>');
-    res.redirect('https://www.gmmoa.com');
+    return res.redirect('https://www.gmmoa.com');
 });
 app.use('/api', subs_1.default);
 https_1.default.createServer(options, app).listen(port, () => {
