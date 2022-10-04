cecfe11921a7a9163f314c5dbf4030d47cb21ce1
Gitakkkk <stom1028@naver.com>
2022-10-05 01:11:00 +0900

    add: 법원별 순환하며 스크래핑 - 점검 이슈로 인해 테스트는 내일 점검이 끝나는대로 진행할 예정

--- src/auction.py
+++ src/auction.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 import re
 import pymongo
 
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+browser = webdriver.Chrome(executable_path='/Users/nagitak/Desktop/gmmoa_back/chromedriver', chrome_options=options)
+browser.get('https://www.courtauction.go.kr/')
+browser.switch_to.frame(browser.find_element(By.NAME, 'indexFrame'))
+
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["gmmoa"]
 collection = db["items"]
@@ -15,37 +23,36 @@ def no_space(text):
     text2 = re.sub('\n\n', '', text1)
     return text2
 
-browser = webdriver.Chrome(executable_path='/Users/nagitak/Desktop/gmmoa_back/chromedriver')
-browser.get('https://www.courtauction.go.kr/')
-browser.switch_to.frame(browser.find_element(By.NAME, 'indexFrame'))
-browser.find_element(By.ID, 'main_btn').click()
-browser.find_element(By.XPATH, '//*[@id="contents"]/div[4]/form[1]/table/thead/tr/th[7]/a[1]').click()
-browser.find_element(By.XPATH, '//*[@id="contents"]/div[4]/form[1]/table/thead/tr/th[7]/a[1]').click()
-
-while (go):
-    for p in range(2, 10):
-        browser.find_element(By.XPATH, f'//*[@id="contents"]/div[4]/form[2]/div/div[1]/a[{p}]/span').click()
-        soup = BeautifulSoup(browser.page_source, 'lxml')
-        item = soup.find_all('tr', attrs={'class':['Ltbl_list_lvl1', 'Ltbl_list_lvl0']})
-        if go == False:
-            break
-        for i in range(len(item)):
-            court = no_space(item[i].find_all('td')[1].text).split()[0]
-            name = no_space(item[i].find_all('td')[1].text).split()[1]
-            purpose = no_space(item[i].find_all('td')[2].text).split()[1]
-            location = no_space(item[i].find_all('td')[3].text).split('[')[0]
-            count = -1
-            for j in range(len(no_space(item[i].find_all('td')[3].text).split())):
-                if no_space(item[i].find_all('td')[3].text).split()[j] == no_space(item[i].find_all('td')[3].text).split()[0]:
-                    count += 1
-            value = item[i].find_all('td')[5].text.split()[0]
-            minimum_cost = item[i].find_all('td')[5].text.split()[1]
-            date = item[i].find_all('td')[6].text.split()[1]
-            if str(date) != today:
-                go = False
+for c in range(1, 61):
+    browser.find_element(By.XPATH, f'//*[@id="idJiwonNm1"]/option[{c}]').click() # 법원별 순환
+    browser.find_element(By.ID, 'main_btn').click()
+    browser.find_element(By.XPATH, '//*[@id="contents"]/div[4]/form[1]/table/thead/tr/th[7]/a[1]').click()
+    browser.find_element(By.XPATH, '//*[@id="contents"]/div[4]/form[1]/table/thead/tr/th[7]/a[1]').click()
+    browser.find_element(By.XPATH, '//*[@id="contents"]/div[4]/form[1]/table/thead/tr/th[7]/a[1]').click()
+    while (go):
+        for p in range(2, 10):
+            browser.find_element(By.XPATH, f'//*[@id="contents"]/div[4]/form[2]/div/div[1]/a[{p}]/span').click()
+            soup = BeautifulSoup(browser.page_source, 'lxml')
+            item = soup.find_all('tr', attrs={'class':['Ltbl_list_lvl1', 'Ltbl_list_lvl0']})
+            if go == False:
                 break
-            status = no_space(item[i].find_all('td')[6].text).split()[1]
+            for i in range(len(item)):
+                court = no_space(item[i].find_all('td')[1].text).split()[0]
+                name = no_space(item[i].find_all('td')[1].text).split()[1]
+                purpose = no_space(item[i].find_all('td')[2].text).split()[1]
+                location = no_space(item[i].find_all('td')[3].text).split('[')[0]
+                count = -1
+                for j in range(len(no_space(item[i].find_all('td')[3].text).split())):
+                    if no_space(item[i].find_all('td')[3].text).split()[j] == no_space(item[i].find_all('td')[3].text).split()[0]:
+                        count += 1
+                value = item[i].find_all('td')[5].text.split()[0]
+                minimum_cost = item[i].find_all('td')[5].text.split()[1]
+                date = item[i].find_all('td')[6].text.split()[1]
+                if str(date) != today:
+                    go = False
+                    break
+                status = no_space(item[i].find_all('td')[6].text).split()[1]
 
-            mydict = { "court": court, "name": name, "purpose": purpose, "location": location, "count": count, "value": value, "minimum_cost": minimum_cost, "date": date, "status": status}
-            collection.insert_one(mydict)
+                mydict = { "court": court, "name": name, "purpose": purpose, "location": location, "count": count, "value": value, "minimum_cost": minimum_cost, "date": date, "status": status}
+                collection.insert_one(mydict)
 browser.quit()
