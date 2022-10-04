e35d843a97635aae9c9524b4fe790e0193385de9
Gitakkkk <stom1028@naver.com>
2022-10-04 15:40:46 +0900

    add: browser.executable_path

--- src/auction.py
+++ src/auction.py
@@ -1,2 +1,6 @@
 # selenium 사용해서 원하는 페이지로 이동 -> 모든 html 크롤링 -> 필요에 따라 정보 추출
 
+from selenium import webdriver
+
+browser = webdriver.Chrome(executable_path='/Users/nagitak/Desktop/gmmoa_back/chromedriver')
+browser.get('https://www.courtauction.go.kr/')
