0d33aabd35332fdaa8d52a324af97d51bd1ced6e
Gitakkkk <stom1028@naver.com>
2022-10-06 06:45:19 +0900

    fix: 2022-10-06

--- src/auction.py
+++ src/auction.py
@@ -11,8 +11,8 @@ options.add_argument("disable-gpu")
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
 db = client["gmmoa"]
-collection = db["2022-10-05"]
-today = '2022.10.19'
+collection = db["2022-10-06"]
+today = '2022.10.20'
 
 def no_space(text):
     text1 = re.sub('&nbsp; | &nbsp;| \n|\t|\r', '', text)
